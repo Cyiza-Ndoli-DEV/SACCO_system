c22a57867fdf1e34a346edeb4e4d50b74023f013
ndjek1 <vndjekornom@gmail.com>
2023-08-02 15:51:20 +0300

    Changed a few things in the performLoanRequest function

--- src/App.java
+++ src/App.java
@@ -14,7 +14,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import java.util.ArrayList;
-import java.util.List;
+//import java.util.List;
 
 // LoanRequest class to store details of each loan request
 class LoanRequest {
